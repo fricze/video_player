6795f846a2d54af1a99d0479343e9835b7c4fa54
Andrzej Fricze <fricza01@heiway.net>
2024-12-21 12:25:37 +0100

    Show duration and progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate ffmpeg_next as ffmpeg;
+use ffmpeg::rescale::TIME_BASE;
+use sdl2::rect::Rect;
 
 extern crate sdl2;
 use ffmpeg::ffi::{avio_seek, AVFormatContext, AVSEEK_SIZE};
 use ffmpeg::util::frame::Video;
+use ffmpeg::util::rational::Rational;
 
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::render::TextureAccess;
@@ -11,14 +14,28 @@ use std::env;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
-fn _format_duration(duration: i64) -> String {
-    let duration_secs = duration as f64 / ffmpeg::ffi::AV_TIME_BASE as f64;
+fn format_duration(duration_secs: f64) -> String {
+    // let duration_secs = duration as f64 / ffmpeg::ffi::AV_TIME_BASE as f64;
 
     let hours = (duration_secs / 3600.0).floor() as u32;
     let minutes = ((duration_secs % 3600.0) / 60.0).floor() as u32;
     let seconds = (duration_secs % 60.0).floor() as u32;
 
     format!("{:02}:{:02}:{:02}", hours, minutes, seconds)
+}
+
+fn fps_to_ms(fps: f64) -> f64 {
+    if fps == 0.0 {
+        panic!("FPS cannot be zero for frame duration calculation");
+    }
+    1000.0 / fps
+}
+
+fn rational_fps_to_ms(rate: Rational) -> f64 {
+    if rate.numerator() == 0 {
+        panic!("FPS numerator cannot be zero for frame duration calculation");
+    }
+    1000.0 * (rate.denominator() as f64 / rate.numerator() as f64)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -44,13 +61,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .ok_or("Could not find a video stream")?
         .index();
 
-    let codec_context = ffmpeg::codec::context::Context::from_parameters(
-        ictx.stream(video_stream_index).unwrap().parameters(),
-    )?;
-    let decoder = codec_context.decoder().video().unwrap();
+    let stream = ictx.stream(video_stream_index).unwrap();
+    let parameters = stream.parameters();
+    let codec_context = ffmpeg::codec::context::Context::from_parameters(parameters)?;
+    let time_base = stream.time_base();
 
-    // let duration = format_duration(input_context.duration());
-    // println!("{}", duration);
+    let avg_frame_rate = stream.avg_frame_rate();
+
+    // println!("avg_frame_rate {}", frame_rate);
+
+    let decoder = codec_context.decoder().video().unwrap();
 
     let sdl_context = sdl2::init().unwrap_or_else(|e| {
         eprintln!("Failed to initialize SDL2: {}", e);
@@ -97,7 +117,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     thread::spawn(move || {
         let mut ictx = ffmpeg::format::input(&input_path_clone).unwrap();
 
-        let position = 93184;
+        let position = 0;
         ictx.seek(position, ..).unwrap();
 
         let avio_ctx = unsafe {
@@ -122,6 +142,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             ictx.stream(video_stream_index).unwrap().parameters(),
         )
         .unwrap();
+
+        let frame_rate = codec_context.frame_rate();
+        let ms_per_frame = {
+            if frame_rate.numerator() == 0 {
+                rational_fps_to_ms(avg_frame_rate)
+            } else {
+                rational_fps_to_ms(frame_rate)
+            }
+        };
+
         let mut decoder = codec_context.decoder().video().unwrap();
         let mut new_frame = ffmpeg::util::frame::Video::empty();
 
@@ -140,8 +170,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                             .or_default()
                             .push(new_frame.clone());
 
-                        // Simulate ~25 FPS (40ms per frame)
-                        std::thread::sleep(std::time::Duration::from_millis(40));
+                        std::thread::sleep(std::time::Duration::from_millis(ms_per_frame as u64));
                     }
                 }
             }
@@ -150,6 +179,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut quit = false;
     let mut event_pump = sdl_context.event_pump()?;
+
+    let ttf_context = sdl2::ttf::init().unwrap_or_else(|e| {
+        eprintln!("Failed to initialize SDL2 TTF: {}", e);
+        std::process::exit(1);
+    });
+    let font = ttf_context.load_font("./WorkSans-Regular.ttf", 16)?;
+
+    let video_duration = ictx.duration();
 
     'main: loop {
         for event in event_pump.poll_iter() {
@@ -174,7 +211,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 // Get the smallest PTS (first in order)
                 if let Some((&pts, frames)) = frames_lock.iter().next() {
                     if let Some(video_frame) = frames.get(0) {
-                        // Update texture with frame data
                         texture
                             .update_yuv(
                                 None,
@@ -189,6 +225,42 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
                         canvas.clear();
                         canvas.copy(&texture, None, None).unwrap();
+
+                        let video_duration = video_duration / ffmpeg::ffi::AV_TIME_BASE as i64;
+                        let pts_in_seconds = pts as f64 * time_base.numerator() as f64
+                            / time_base.denominator() as f64;
+
+                        // Calculate progress
+                        let progress = pts_in_seconds as f64 / video_duration as f64 * 100.0;
+
+                        // println!("pts_in_sec : {}", pts_in_seconds);
+                        // println!("duration : {}", video_duration);
+                        // println!("progress: {}", progress);
+
+                        let width = decoder.width() as f64 / 100.0 * (progress as f64);
+
+                        let progress_rect =
+                            Rect::new(0, decoder.height() as i32 - 25, width as u32, 5);
+                        canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 255, 0));
+                        canvas.fill_rect(progress_rect)?;
+
+                        // Render duration text
+                        let surface = font
+                            .render(&format_duration(video_duration as f64))
+                            .blended(sdl2::pixels::Color::RGB(255, 255, 255))?;
+
+                        let texture = texture_creator.create_texture_from_surface(&surface)?;
+                        canvas.copy(
+                            &texture,
+                            None,
+                            Some(Rect::new(
+                                decoder.width() as i32 - 100,
+                                decoder.height() as i32 - 20,
+                                100,
+                                20,
+                            )),
+                        )?;
+
                         canvas.present();
 
                         // Remove the frame after it's been displayed or move to the next if there are multiple at the same PTS
@@ -205,31 +277,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
             }
         }
-
-        // {
-        //     let mut frames_lock = video_frames.lock().unwrap();
-        //     if let Some(video_frame) = frames_lock.pop() {
-        //         texture
-        //             .update_yuv(
-        //                 None,
-        //                 video_frame.data(0),
-        //                 video_frame.stride(0) as usize,
-        //                 video_frame.data(1),
-        //                 video_frame.stride(1) as usize,
-        //                 video_frame.data(2),
-        //                 video_frame.stride(2) as usize,
-        //             )
-        //             .unwrap_or_else(|e| {
-        //                 eprintln!("Failed to update SDL2 texture: {}", e);
-        //                 std::process::exit(1);
-        //             });
-
-        //         canvas.clear();
-        //         canvas.copy(&texture, None, None).unwrap();
-
-        //         canvas.present();
-        //     }
-        // }
     }
 
     println!("Playback Finished!");
